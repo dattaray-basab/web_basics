44ba28df182011433503c5bd3902dfef97331c8b
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2022-11-09 15:06:37 -0800

    0.1.0

--- pw.js
+++
@@ -1,9 +0,0 @@
-function isValid() {
-    var password = document.getElementById('password').value;
-    if (password == "123")
-        // top.location.href="./file.pdf";
-        console.log("its ok!")
-    else 
-        console.log("its NOT ok!")
-        window.location.reload();
-}
